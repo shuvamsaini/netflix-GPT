3f34930bf79e0ba88331731f2111eb3562ae1f79
Shuvam Saini <topoftheworld251@gmail.com>
2024-02-15 13:43:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+        <img src="logo.svg" className="h-[80px]" alt="" />
+    </div>
+  )
+}
+
+export default Header;
